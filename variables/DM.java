6bda22fd015a9b55bdbacd1b79d9b5a280c8baed
codebyprajjwal02 <prajjwalsingh505@gmail.com>
2026-02-18 21:59:26 +0530

    Divide(quotient) and modulo(remainder)

---
+++ variables/DM.java
@@ -0,0 +1,14 @@
+public class DM {
+    public static void main(String[] args){
+        int x=12;
+        int y=18;
+        int a=x/y;
+        int b=y/x;
+        int c=x%y;
+        int d=y%x;
+        System.out.println(a);
+        System.out.println(b);
+        System.out.println(c);
+        System.out.println(d);
+    }   
+}
