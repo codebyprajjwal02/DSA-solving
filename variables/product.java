4b514738677e43560e3b14abe1e086b1d5efcfda
codebyprajjwal02 <prajjwalsingh505@gmail.com>
2026-02-18 21:52:20 +0530

    product problem

---
+++ variables/product.java
@@ -0,0 +1,8 @@
+public class product {
+    public static void main(String[] args){
+        int x=12;
+        int y=15;
+        int product=x*y;
+        System.out.print("product of "+x+" and "+y+" is "+product);
+    }    
+}
