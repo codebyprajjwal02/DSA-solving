48f54d09fb61749f28010adc7309cbc2b29f8c20
codebyprajjwal02 <prajjwalsingh505@gmail.com>
2026-02-18 21:47:12 +0530

    sum prblm

---
+++ variables/sum.java
@@ -0,0 +1,8 @@
+public class sum {
+    public static void main(String[] args){
+        int a=15;
+        int b=50;
+        int sum=a+b;
+        System.out.println("sum of "+a+" and "+b+" is "+sum);
+    }
+}
