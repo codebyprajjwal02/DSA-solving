9bd0648a30569fbb3b8845c263d523b6d0585532
codebyprajjwal02 <prajjwalsingh505@gmail.com>
2026-02-24 11:13:38 +0530

    combine methods

---
+++ variables/combine.java
@@ -0,0 +1,15 @@
+public class combine {
+     public static void main(String[] args){
+        int x=12;
+        int y=15;
+        int sum=x+y;
+        int difference=x-y;
+        int division=x/y;
+        int modulus=x%y;
+        int product=x*y;
+        int exp=(x*y)*(x+y);
+        System.out.print(sum +"\n" +difference+ "\n"+ division+ "\n" +modulus +"\n" +product +"\n" + exp);
+    }    
+}
+    
+
