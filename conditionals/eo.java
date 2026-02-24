bb5085932aa7a107d7dd58e5602d18f031cc64b7
codebyprajjwal02 <prajjwalsingh505@gmail.com>
2026-02-24 11:22:41 +0530

    even odd prblm

---
+++ conditionals/eo.java
@@ -0,0 +1,13 @@
+public class eo {
+    public static void main(String[] args){
+        int x=45;
+        if (x%2==0){
+            System.out.println(x+" is even.");
+        }
+        else{
+            System.out.println(x+" is odd.");
+        }
+        System.out.println("Smart work is better than Hardwork");
+    }
+    
+}
