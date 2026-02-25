b44a629b7ccbb197caf3522afd8870ba00ced255
codebyprajjwal02 <prajjwalsingh505@gmail.com>
2026-02-25 07:57:34 +0530

    while Loop

---
+++ Loops/wile.java
@@ -0,0 +1,10 @@
+public class wile {
+    public static void main(String[] args){
+        int i=1;
+        while(i<=15){
+            System.out.print(i+"\n");
+            i++;
+        }
+        System.out.print("Done.");
+    }
+}
