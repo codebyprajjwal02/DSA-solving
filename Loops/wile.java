b87a4a1f58b675037e41e839846b1e765e0ffe0f
codebyprajjwal02 <prajjwalsingh505@gmail.com>
2026-02-25 08:04:06 +0530

    while Loop 2nd

--- Loops/wile.java
+++ Loops/wile.java
@@ -1,10 +1,13 @@
 public class wile {
     public static void main(String[] args){
-        int i=1;
-        while(i<=15){
+        // int i=1;
+        // while(i<=15){
+        //     System.out.print(i+"\n");
+        //     i++;
+        for(int i=1;i<=15;i++){
             System.out.print(i+"\n");
-            i++;
         }
         System.out.print("Done.");
     }
 }
+
